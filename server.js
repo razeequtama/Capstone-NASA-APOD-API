11155cf0d7c616c27233bc1cdb15cc3aee7eff2a
Raziq <rabbaniu74m4@gmail.com>
2026-07-13 19:47:40 +0700

    Connect Server with API

---
+++ server.js
@@ -0,0 +1,45 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import dotenv from 'dotenv';
+import axios from 'axios';
+
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+const PORT = process.env.PORT || 3000;
+const API_URL = process.env.URL_ENDPOINT;
+const API_KEY = process.env.API_KEY;
+
+// Set EJS as template engine
+app.set("view engine", "ejs");
+
+// Set views directory
+app.set("views", path.join(__dirname, "views"));
+
+// Set static default directory to public/
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+const config = {
+    params: {
+        api_key: API_KEY
+    }
+}
+
+app.get("/", async (r, s) => {
+    try {
+        const response = await axios.get(API_URL, config);
+        const result = response.data;
+        console.log(result);
+        s.send(result);
+    } catch (error) {
+        s.send("Failed to fetch.");
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
